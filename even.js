b951447af8809c2652b93f8f5488508bde0817ef
shubh9713 <shubhammddp@gmail.com>
2023-01-17 20:59:29 +0530

    check even

---
+++ even.js
@@ -0,0 +1,9 @@
+ for(let i=1; i<=num; i++){
+        let sum= 0;
+     for(let j=1; j<=i; j++){
+         if(j%2==0){
+             sum= sum+i;
+         }
+         
+     }   console.log(sum)
+    
