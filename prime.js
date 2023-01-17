2c07408723308539a97e776d090ffe611217b844
Supriya Kumari <supriyayout@gmail.com>
2023-01-17 20:44:00 +0530

    prime number

---
+++ prime.js
@@ -0,0 +1,12 @@
+let factor=0;
+   for(let i=1;i<=num;i++){
+       if(num%i==0){
+           factor++;
+       }
+   }
+   if(factor==2){
+       console.log("Yes");
+   }
+   else{
+     console.log("No");  
+   }
